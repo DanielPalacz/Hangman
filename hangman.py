69169290413b5dbbe3960c01365f4d27d5f18c67
DanielPalacz <daniel.palacz@gmail.com>
2020-11-06 14:30:15 +0100

    changed game logic

--- hangman.py
+++ hangman.py
@@ -1,16 +1,49 @@
 """Hangman game implementation.
 
-(stand-alone program / module) - more DocString details - line_1
-(stand-alone program / module) - more DocString details - line_2
-...
-(stand-alone program / module) - more DocString details - line_n
+Usage:
+    hangman.py -demo   - running Hangman game in Demo mode
 """
 
 from inputimeout import inputimeout, TimeoutOccurred
 import sys
 
 
-class HangmanGame():
+class HangmanPlayer:
+    """HangmanPlayer Class layer Doc-string"""
+
+    def __init__(self, name):
+        """Constructor for HangmanPlayer Class objects"""
+        self.name = name
+
+    def provide_character(self):
+        """ ... """
+        while True:
+            char_input = ""
+            try:
+                char_input = inputimeout(prompt="\nProvide single character "
+                                                "(in 5 seconds): ", timeout=5)
+            except TimeoutOccurred:
+                char_input = ""
+
+            if len(char_input) == 1:
+                break
+            elif len(char_input) == 0:
+                char_input = ""
+                break
+            else:
+                print("Please enter only one character")
+        return char_input
+
+    def guess_word(self):
+        """  ... """
+        try:
+            return inputimeout(prompt="\nGuess word in (in 10 seconds): ",
+                               timeout=10)
+        except TimeoutOccurred:
+            return ""
+
+
+class HangmanGame:
     """Summary of HangmanGame Class
 
     Longer class information....
@@ -40,40 +73,35 @@ class HangmanGame():
         """
         self.guessed_word = guessed_word
         self.guessing_state = len(guessed_word) * "*"
-        self.last_character = ""
-        self.guessed_chars = {"NULL": 0}
+        self.guessed_chars = {}
 
     def __str__(self):
         return "Hello it is __str__ (special method) output of: " \
                "SingleHangmanPlayer classes"
 
-    def update_game_state(self):
+    def is_finished(self):
+        test_table = [1 if c1 not in self.guessed_chars else 0 for c1 in
+                      self.guessed_word]
+        if sum(test_table) == 0:
+            return True
+        else:
+            return False
+
+    def update_game_state(self, character: str) -> None:
         """Updates game state.
 
         More method information...
         More method information...
         """
 
-        if self.last_character not in self.guessed_chars.keys():
-            self.guessed_chars[self.last_character] = 0
-        self.guessed_chars[self.last_character] += 1
+        if character not in self.guessed_chars.keys():
+            self.guessed_chars[character] = 0
+        self.guessed_chars[character] += 1
 
-        if self.last_character in self.guessing_state:
-            pass
-        elif self.last_character in self.guessed_word:
-            last_character_distribution_in_word = []
-            temp = ''
-            for elem in enumerate(self.guessed_word):
-                if self.last_character == elem[1]:
-                    last_character_distribution_in_word.append(elem[0])
-            for elem in enumerate(self.guessing_state):
-                if elem[0] in last_character_distribution_in_word:
-                    temp = temp + self.last_character
-                else:
-                    temp = temp + elem[1]
-            self.guessing_state = temp
+        self.guessing_state = "".join([c1 if c1 in self.guessed_chars else "*"
+                                       for c1 in self.guessed_word])
 
-    def run_game(self, player):
+    def run_game(self, player: HangmanPlayer) -> None:
         """Runs game.
 
         More method information...
@@ -83,11 +111,12 @@ class HangmanGame():
         print("""
         Hello in Hangman Game!
         """)
-        while self.guessed_word != self.guessing_state:
+        print(self.is_finished())
+        while not self.is_finished():
             print("The state of guessing is following:", self.guessing_state)
-            self.last_character = player.provide_character()
-            self.update_game_state()
-            if self.guessing_state == self.guessed_word:
+            self.update_game_state(player.provide_character())
+
+            if self.is_finished():
                 print("Super! You guessed the word:", self.guessed_word)
                 print("\nIt is end of Hangman Game. Thank You!!!")
                 break
@@ -97,57 +126,21 @@ class HangmanGame():
             if guess_try == self.guessed_word:
                 print("Hurra you guessed. The word is:", guess_try)
                 self.guessing_state = guess_try
-            elif not guess_try:
-                pass
+                break
+            else:
+                print("\nUnfortunately you did not guess.\n\n")
         else:
             print("\nIt is end of Hangman Game. Thank You!!!")
 
-#    print(player1.__doc__)
-#    print(player1)
-#    print(SingleHangmanPlayer.__doc__)
-#    print(player1.__init__.__doc__)
-
-
-class HangmanPlayer():
-    """HangmanPlayer Class layer Doc-string"""
-
-    def __init__(self, name):
-        """Constructor for HangmanPlayer Class objects"""
-        self.name = name
-
-    def provide_character(self):
-        """ ... """
-        while True:
-            char_input = ""
-            try:
-                char_input = inputimeout(prompt="\nProvide single character "
-                                                "(in 5 seconds): ", timeout=5)
-            except TimeoutOccurred:
-                char_input = ""
-
-            if len(char_input) == 1:
-                break
-            elif len(char_input) == 0:
-                char_input = "NULL"
-                break
-            else:
-                print("Please enter only one character")
-        return char_input
-
-    def guess_word(self):
-        """  ... """
-        guess_attempt = input("\nDo you want to guess the word? "
-                              "Type word if 'Yes'. 'No', click enter. ")
-
-        return guess_attempt
-
 
 if __name__ == "__main__":
-    if "-h" in sys.argv:
+    if len(sys.argv) == 1:
         print(__doc__)
         exit()
-
-    janek = HangmanPlayer("Janek")
-    game = HangmanGame("kot")
-    game.run_game(janek)
-    print("\nGame stats:\n", game.guessed_chars)
+    elif "-demo" in sys.argv:
+        janek = HangmanPlayer("Janek")
+        game = HangmanGame("kot")
+        game.run_game(janek)
+        print("\nGame stats:\n", game.guessed_chars)
+    else:
+        print("\nThe given way of running Hangman game is not implemented")
