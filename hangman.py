41dd6d7aed0cfadfbbd0e2c051920c518bd404e8
DanielPalacz <daniel.palacz@gmail.com>
2020-11-06 20:15:17 +0100

    next improvements

--- hangman.py
+++ hangman.py
@@ -17,14 +17,22 @@ import argparse
 
 
 class HangmanPlayer:
-    """HangmanPlayer Class layer Doc-string"""
+    """HangmanPlayer Class implementation.
 
-    def __init__(self, name):
-        """Constructor for HangmanPlayer Class objects"""
+    Attributes:
+        name:
+            - name of the player
+    """
+
+    def __init__(self, name: str) -> None:
+        """Inits HangmanPlayer object with name."""
         self.name = name
 
     def provide_character(self):
-        """ ... """
+        """Implements providing character by the player.
+
+        Player hs 5 seconds to provide character.
+        """
         while True:
             try:
                 char_input = inputimeout(prompt="\nProvide single character "
@@ -43,7 +51,10 @@ class HangmanPlayer:
         return char_input
 
     def guess_word(self):
-        """  ... """
+        """Implements guessing word by the player.
+
+        Player has 10 seconds to guess the word.
+        """
         try:
             return inputimeout(prompt="\nGuess word in (in 10 seconds): ",
                                timeout=10)
@@ -52,23 +63,23 @@ class HangmanPlayer:
 
 
 class HangmanGame:
-    """Summary of HangmanGame Class
+    """HangmanGame Class implementation.
 
-    Longer class information....
-    Longer class information....
-
+    Implements Hangman-like game, based only on guessing the word.
     Attributes:
         guessed_word:
             - string contains word to be guessed
         guessing_state:
             - string contains state of Game
             - it is '***' if guessed_word is initialised as 'cat'
-        last_character:
-            - string indicates the latest input provided by User
-            - it is always single character apart of situation when:
-               User did not provide input then it is set by Game to "NULL"
         guessed_chars:
             - dictionary contains appearing the given characters or NULL
+        players:
+            - list with players (objects of HangmanPlayer)
+        winner:
+            - name of player who won the game
+        round_number:
+            - number tracking rounds of the game
     """
 
     def __init__(self, guessed_word, *gamers):
@@ -85,12 +96,14 @@ class HangmanGame:
         self.players = [HangmanPlayer(gamer) for gamer in gamers]
         self.winner = None
         self.round_number = 0
+        self.results = {gamer: dict() for gamer in gamers}
 
     def __str__(self):
         return "Hello it is __str__ (special method) output of: " \
                "SingleHangmanPlayer classes"
 
     def is_finished(self):
+        """Check if the game is finished."""
         test_table = [1 if c1 not in self.guessed_chars else 0 for c1 in
                       self.guessed_word]
         if sum(test_table) == 0:
@@ -101,11 +114,7 @@ class HangmanGame:
             return False
 
     def update_game_state(self, character: str) -> None:
-        """Updates game state.
-
-        More method information...
-        More method information...
-        """
+        """Updates game state."""
 
         if character not in self.guessed_chars.keys():
             self.guessed_chars[character] = 0
@@ -114,11 +123,15 @@ class HangmanGame:
         self.guessing_state = "".join([c1 if c1 in self.guessed_chars else "*"
                                        for c1 in self.guessed_word])
 
-    def run_game(self):
-        """Runs Hangman game.
+    def end_game(self, the_current_player):
+        print("Super!", the_current_player, "- you guessed the word:",
+              self.guessed_word)
+        self.winner = the_current_player
+        self.round_number += 1
 
-        More information...
-        """
+    def run_game(self):
+        """Runs Hangman game."""
+
         print("***********************************************************\n")
         print(*[x.name for x in self.players], "- Hello in Hangman Game!\n\n")
 
@@ -132,24 +145,18 @@ class HangmanGame:
 
                 self.update_game_state(player.provide_character())
                 if self.is_finished():
-                    print("Super!", player.name, "- you guessed the word:",
-                          self.guessed_word)
-                    self.winner = player.name
+                    self.end_game(player.name)
                     break
-                print("\nThe state of guessing is following:",
-                      self.guessing_state)
 
+                print("\nThe state of game is following:",self.guessing_state)
                 guess_try = player.guess_word()
                 if guess_try == self.guessed_word:
-                    print("Hurra", player.name,
-                          "you guessed. The word is:", guess_try)
-                    self.guessing_state = guess_try
-                    self.winner = player.name
+                    self.end_game(player.name)
                     break
                 else:
                     print("\nUnfortunately you did not guess.\n\n")
 
-            self.round_number += 1
+                self.round_number += 1
 
         else:
             print("\nIt is end of Hangman Game number XYZ. "
@@ -178,3 +185,4 @@ if __name__ == "__main__":
         game = HangmanGame("kot", *args.player)
         game.run_game()
         print("\nGame stats:\n", game.guessed_chars)
+        print("\nGame results:\n", game.results)
