48fafc1aeb477aee9223cc9b7d7f95dd2ef4b0a5
DanielPalacz <daniel.palacz@gmail.com>
2020-11-15 02:54:28 +0100

    Continuing work with unittest.

--- unittest_testing/test_db.py
+++ unittest_testing/test_db.py
@@ -7,20 +7,66 @@ from db import initialize_db
 
 class TestDB(unittest.TestCase):
 
-    def test__initialize_db(self):
-        """Tests if A-Sqlite3 db can be initialized."""
-
+    @classmethod
+    def setUpClass(cls):
+        """Class level fixture"""
         # Setting up test env:
-        dbname = "Test_DB.sqlite"
-        if os.path.isfile(dbname):
-            os.remove(dbname)
-        test_conn = sqlite3.connect(dbname)
-        test_cursor = test_conn.cursor()
-        self.assertTrue(test_conn and test_cursor, "Setting test env failed.")
+        cls._dbname = "Test_DB.sqlite"
+        if os.path.isfile(cls._dbname):
+            os.remove(cls._dbname)
 
-        self.assertTrue(initialize_db(test_cursor),
-                        "Sqlite3 DB could not be initialized")
+        cls._conn = sqlite3.connect(cls._dbname)
+        cls._cursor = cls._conn.cursor()
+        cls._stats_table_defined = {"game_id": "INTEGER",
+                                    "round_id": "INTEGER",
+                                    "player": "TEXT",
+                                    "guess": "TEXT",
+                                    "winner": "TEXT"}
 
+    def setUp(self):
+        """Test level fixture"""
+        pass
+
+    def test__initialize_db1(self):
+        # 1st line of Docstring goes to verbose mode ... (-v)
+        """
+        Tests if A-Sqlite3 db can be initialized."""
+        initialize_db(TestDB._cursor)
+        TestDB._conn.commit()
+        self.assertTrue(os.path.isfile(TestDB._dbname), "DB was not created.")
+
+    def test__initialize_db2(self):
+        """
+        ...
+        """
+        TestDB._cursor.execute("PRAGMA table_info(stats)")
+        test_stats = {x[1]: x[2] for x in TestDB._cursor.fetchall()}
+        for y in TestDB._stats_table_defined.keys():
+            with self.subTest(y):
+                self.assertEqual(TestDB._stats_table_defined[y], test_stats[y],
+                                 "column definition doesnt match with setup")
+
+    def tearDown(self):
+        """Test level fixture"""
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """Class level fixture"""
+        cls._conn.close()
+
+
+def setUpModule():
+    """Module level fixture"""
+    print("")
+    mod_name = "'" + __name__ + "'"
+    print("Tests for module:", mod_name, "are starting.\n")
+
+
+def tearDownModule():
+    """Module level fixture"""
+    print("")
+    print("End of Tests for module:", __name__)
 
 
 if __name__ == '__main__':
